9dd49733d2294030a907f9bbdc9655a543894d49
Pankaj17ABD <rwtpnkj1718@gmail.com>
2023-04-11 00:42:28 +0530

    Logical Operator and ternary Operator

---
+++ day3js/problem11.js
@@ -0,0 +1,8 @@
+let char="b"
+
+if(char!=="a" ||char!=="e" ||char!=="i" ||char!=="o" ||char!=="u"){
+  console.log("consonant")
+}else if(char=="a" ||char=="e" ||char=="i" ||char=="o" ||char=="u"){
+  console.log("Not an consonant")
+}
+
